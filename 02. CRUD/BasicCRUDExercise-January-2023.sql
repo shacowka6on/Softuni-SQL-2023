18d7f5fd15881b57853231e21e55c04460296341
sashi <92218265+shacowka6on@users.noreply.github.com>
2023-01-27 21:23:32 +0200

    Add files via upload

---
+++ 02. CRUD/BasicCRUDExercise-January-2023.sql
@@ -0,0 +1,173 @@
+USE [SoftUni]
+
+GO
+
+-- Problem 02
+SELECT * FROM [Departments]
+
+-- Problem 03
+SELECT [Name]
+  FROM [Departments]
+
+ -- Problem 04
+ SELECT [FirstName], 
+		[LastName], 
+		[Salary]
+   FROM [Employees]
+
+-- Problem 05
+SELECT [FirstName], 
+	   [MiddleName], 
+	   [LastName]
+  FROM [Employees]
+
+-- Problem 06
+  SELECT CONCAT([FirstName], '.', [LastName], '@softuni.bg')
+	  AS [Full Email Address]
+    FROM [Employees]
+
+-- Problem 07
+SELECT DISTINCT [Salary]
+           FROM [Employees]
+
+-- Problem 08
+SELECT * 
+  FROM [Employees] 
+ WHERE [JobTitle] IN ('Sales Representative')
+
+-- Problem 09
+SELECT [FirstName], [LastName], [JobTitle] 
+  FROM [Employees] 
+ WHERE [Salary] BETWEEN 20000 AND 30000
+
+ 
+-- Problem 10
+SELECT CONCAT([FirstName],' ',[MiddleName],' ',[LastName])
+    AS [FullName]
+  FROM [Employees] 
+ WHERE [Salary] = 14000 OR [Salary] = 25000 OR [Salary] = 12500 OR [Salary] = 23600
+
+ -- Problem 11
+SELECT [FirstName], 
+	   [LastName]
+  FROM [Employees] 
+ WHERE [ManagerID] IS NULL
+ 
+  -- Problem 12
+SELECT [FirstName], 
+	   [LastName],
+	   [Salary]
+  FROM [Employees] 
+ WHERE [Salary] >= 50000 
+ ORDER
+	BY [Salary] DESC
+
+-- Problem 13
+SELECT TOP 5 
+       [FirstName], 
+	   [LastName]
+  FROM [Employees] 
+ WHERE [Salary] >= 50000
+ ORDER
+	BY [Salary] DESC 
+
+-- Problem 14
+SELECT [FirstName], 
+	   [LastName]
+  FROM [Employees] 
+ WHERE [DepartmentID] != 4
+
+-- Problem 15
+SELECT *
+  FROM [Employees] 
+ ORDER
+	BY [Salary] DESC  ,
+	   [FirstName]    ,
+	   [LastName] DESC,
+	   [MiddleName]
+
+-- Problem 16
+GO
+
+CREATE VIEW [V_EmployeesSalaries]
+		 AS
+		    (
+				SELECT [FirstName], 
+					   [LastName],
+					   [Salary]
+				FROM   [Employees]
+		    )
+
+			GO
+--Problem 17
+GO
+
+CREATE VIEW [V_EmployeeNameJobTitle]
+		 AS
+		    (
+				SELECT CONCAT([FirstName], ' ', [MiddleName],' ',[LastName])
+				  AS   [FullName],
+					   [JobTitle]
+				FROM   [Employees]
+		    )
+			GO
+
+--Problem 18
+SELECT DISTINCT [JobTitle]
+		   FROM [Employees]
+
+--Problem 19
+SELECT TOP 10 *
+      FROM [Projects]
+  ORDER BY [StartDate] ASC,
+  [Name] ASC
+
+  
+--Problem 20
+SELECT TOP 7 [FirstName], 
+             [LastName] , 
+			 [HireDate]
+        FROM [Employees]
+    ORDER BY [HireDate] DESC
+
+--Problem 21
+SELECT [DepartmentID] 
+  FROM [Departments]
+WHERE  [Name] IN ('Engineering', 'Tool Design', 'Marketing', 'Information Services')
+
+UPDATE [Employees]
+   SET [Salary] *= 1.12
+ WHERE [DepartmentID] IN (1, 2, 4, 11)
+
+ SELECT [Salary] 
+   FROM [Employees]
+
+--Problem 22
+USE [Geography]
+
+GO
+
+SELECT [PeakName] FROM [Peaks] ORDER BY [PeakName] ASC
+
+--Problem 23
+SELECT TOP 30 [CountryName], 
+              [Population] 
+         FROM [Countries] 
+        WHERE [ContinentCode] IN ('EU')
+     ORDER BY [Population] DESC
+
+--Problem 24
+SELECT [CountryName], [CountryCode],
+CASE
+  WHEN [CurrencyCode] IN ('EUR') THEN 'Euro'
+  ELSE 'Not Euro'
+END AS [Currency]
+  FROM [Countries]
+ORDER BY [CountryName] ASC
+
+--Problem 25
+USE [Diablo]
+
+GO
+
+SELECT [Name] FROM [Characters] ORDER BY [Name] ASC
